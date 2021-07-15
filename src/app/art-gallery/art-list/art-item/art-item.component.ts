d2486dfee8af916d69637c72d6f7f4eb05bffea4
Mahesh Biradar <bmahesh66@gmail.com>
2021-07-15 12:52:47 +0530

    Added required components

---
+++ src/app/art-gallery/art-list/art-item/art-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-art-item',
+  templateUrl: './art-item.component.html',
+  styleUrls: ['./art-item.component.css']
+})
+export class ArtItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/art-shopping-cart/art-shopping-cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-art-shopping-cart',
+  templateUrl: './art-shopping-cart.component.html',
+  styleUrls: ['./art-shopping-cart.component.css']
+})
+export class ArtShoppingCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
