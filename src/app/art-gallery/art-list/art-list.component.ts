9230cfcb9b40c518c5bf49f6e197e268040f4923
Mahesh Biradar <bmahesh66@gmail.com>
2021-07-15 14:20:48 +0530

    Added Movk Data for the components

--- src/app/art-gallery/art-list/art-list.component.ts
+++ src/app/art-gallery/art-list/art-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Art } from '../models/art.model';
 
 @Component({
   selector: 'app-art-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./art-list.component.css']
 })
 export class ArtListComponent implements OnInit {
+  arts: Art[] = [
+    new Art("Test1 Art", "This is test description", "https://lh3.googleusercontent.com/proxy/aKU-MNsNF_figPwJJs917EPCAdVhYxINX6K35ShbaTVxVbeIDNtq6ck4D_kn2xr3-iXL43l2xF-EYAMmjXzOStGT1v7ssLA"),
+    new Art("Test1 Art", "This is test description", "https://www.graphicsprings.com/filestorage/stencils/1f4e948fcfc9977ee6fb567bd815132d.png?width=500&height=500"),
+  ]
 
   constructor() { }
 
--- src/app/art-shopping-cart/art-shopping-cart.component.ts
+++ src/app/art-shopping-cart/art-shopping-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ArtMeterial } from '../art-gallery/models/art-material-model';
 
 @Component({
   selector: 'app-art-shopping-cart',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./art-shopping-cart.component.css']
 })
 export class ArtShoppingCartComponent implements OnInit {
-
+  artMaterials: ArtMeterial[] = [
+    new ArtMeterial("Test1", 300),
+    new ArtMeterial("Test2", 600)
+  ];
   constructor() { }
 
   ngOnInit(): void {
